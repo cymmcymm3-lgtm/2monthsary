f1d3012ae3577253f753df51382f160ccf4c5f7f
cymmcymm3-lgtm <cymmcymm3@gmail.com>
2026-09-12 02:07:31 +0800

    Add website files

---
+++ manifest.js
@@ -0,0 +1,8 @@
+window.PHOTOS = [
+  { src: "photos/01.jpg", caption: "The first time I knew." },
+  { src: "photos/02.jpg", caption: "That laugh of yours." },
+  { src: "photos/03.jpg", caption: "Lazy afternoons." },
+  { src: "photos/04.jpg", caption: "My favorite view is you." },
+  { src: "photos/05.jpg", caption: "Always, always us." },
+  { src: "photos/06.jpg", caption: "To many more." },
+];
